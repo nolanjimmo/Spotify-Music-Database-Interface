3ec7ccd7b787ee9dab2be6f4bda4d632bb6f355a
nolanjimmo <barcafan12345@gmail.com>
2021-02-12 16:22:24 -0500

    Most of SQL functionality
    Most of the SQL functionality is here, it may need some tweaking in terms of combinations of commands. I also have a connection created, but I have not made a LoadData() function

--- Team3WarmUpProject.py
+++ Team3WarmUpProject.py
@@ -5,8 +5,15 @@
 # Delaney Sullivan
 
 # main program holds the interface of the program
+import sqlite3
+from sqlite3 import Error
 
 def main():
+    cursor = createConnection()
+    doubleCommandQuery('artists', 'genre', "Alesso", cursor)
+    doubleCommandQuery('songs', 'artist' , 'Under Control', cursor)
+    doubleCommandQuery('artists', 'title', 'Swedish House Mafia', cursor)
+
     #Change this to be << but this is easier to read while testing
     command = input("Please enter a command: ")
 
@@ -39,6 +46,35 @@ def main():
         command = input("Please enter a command: ")
         commandList = command.split()
         commandCall = commandList[0]
+
+
+##########Database Interaction#########################
+
+#alternate way to access the database using a single and double command query
+#I am going to comment out the connection to the database right now so we don't get any messy errors when we run for now
+def createConnection():
+    connection = None
+    try:
+        connection = sqlite3.connect('SongsArtistsDB.db')
+    except Error as e:
+        print(e)
+    finally:
+        if connection:
+            return connection.cursor()
+
+def doubleCommandQuery(command1, command2, desiredData, curs):
+    if command1 == "artists" and command2 != "title" and command2 != "album":
+        curs.execute("SELECT %s FROM %s WHERE name = '%s'" % (command2, command1, desiredData))
+    elif command1 == "songs":
+        curs.execute("SELECT %s FROM %s WHERE title = '%s'" % (command2, command1, desiredData))
+    else:
+        if command2 == "title":
+            curs.execute("SELECT title FROM songs INNER JOIN artists ON songs.artist = artists.name WHERE "
+                         "songs.artist = '%s'" % desiredData)
+
+    rows = curs.fetchall()
+    for row in rows:
+        print(row[0])
 
 
 #different functions for each SQL call
